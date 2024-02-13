f14ac77c20812e90727244bbcaf070fa5b00fe5c
Mpp216 <mariappereira96@gmail.com>
2024-02-13 11:40:23 +0100

    new changes

---
+++ src/Footer.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <p>
+        This project was coded by
+        <a href="https://github.com/Mpp216" target="_blank">
+          Maria Preto Pereira
+        </a>
+        and is
+        <a href="https://github.com/Mpp216/meteo-app" target="_blank">
+          on GitHub
+        </a>
+        and
+        <a href="https://meteo-tempo-app.netlify.app/" target="_blank">
+          hosted on Netlify
+        </a>
+      </p>
+    </div>
+  );
+}
