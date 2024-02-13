3ba1b381894e56efdf02c8a37e0c390b4388e508
Mpp216 <mariappereira96@gmail.com>
2024-02-13 11:50:32 +0100

    Update links

--- src/Footer.js
+++ src/Footer.js
@@ -6,15 +6,15 @@ export default function Footer() {
     <div className="Footer">
       <p>
         This project was coded by
-        <a href="https://github.com/Mpp216" target="_blank">
+        <a href="https://github.com/Mpp216" target="/">
           Maria Preto Pereira
         </a>
         and is
-        <a href="https://github.com/Mpp216/meteo-app" target="_blank">
+        <a href="https://github.com/Mpp216/my-app" target="/">
           on GitHub
         </a>
         and
-        <a href="https://meteo-tempo-app.netlify.app/" target="_blank">
+        <a href="https://meteo-tempo-app.netlify.app/" target="/">
           hosted on Netlify
         </a>
       </p>
