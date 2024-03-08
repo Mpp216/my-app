69da6adeb124bcbb38bbb0440e5e11e4ca4b3ef1
Mpp216 <mariappereira96@gmail.com>
2024-03-08 13:11:09 +0100

    Create new component and show temperature in different metrics

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
@@ -23,12 +24,7 @@ export default function WeatherInfo(props) {
               className="float-left"
             />
             <div className="float-left">
-              <strong className="temperature">
-                {Math.round(props.data.temperature)}
-              </strong>
-              <span className="units">
-                <a href="/">°C</a> | <a href="/">°F</a>
-              </span>
+              <WeatherTemperature celsius={props.data.temperature} />
             </div>
           </div>
         </div>
